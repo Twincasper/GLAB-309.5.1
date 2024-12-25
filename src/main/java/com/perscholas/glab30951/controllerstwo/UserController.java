47c00989953ba666042201b01517b86df619fa30
Twincasper <justinwatson651@gmail.com>
2024-12-25 18:10:40 -0500

    Implemented model, modelandview and @modelattribute from GLAB-309.5.3

--- src/main/java/com/perscholas/glab30951/controllerstwo/UserController.java
+++ src/main/java/com/perscholas/glab30951/controllerstwo/UserController.java
@@ -1,8 +1,10 @@
 package com.perscholas.glab30951.controllerstwo;
 
 import com.perscholas.glab30951.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
@@ -10,7 +12,7 @@ import org.springframework.ui.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     @PostMapping("/users")
     public void printData(@RequestBody User user){
@@ -18,12 +20,19 @@ public class UserController {
     }
 
     @ModelAttribute("countries")
-    public List< String > getUserCountries() {
-        List < String > countries = new ArrayList<>();
+    public List<String> getUserCountries() {
+        List<String> countries = new ArrayList<>();
         countries.add("UK");
         countries.add("USA");
         countries.add("Japan");
         return countries;
+    }
+
+    @GetMapping("/Home")
+    public String home(@ModelAttribute("countries") List<String> countries, Model model) {
+        countries.add("Australia");
+        countries.add("Canada");
+        return "Home";
     }
 
     @ModelAttribute
