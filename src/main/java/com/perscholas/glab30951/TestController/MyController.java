3b9f709fa09a9a6f51158466f6e3b9dfd44f12c2
Twincasper <justinwatson651@gmail.com>
2024-12-25 17:23:25 -0500

    Implemented User model and corresponding controller for GLAB 5.2

--- src/main/java/com/perscholas/glab30951/TestController/MyController.java
+++ src/main/java/com/perscholas/glab30951/TestController/MyController.java
@@ -1,4 +1,21 @@
 package com.perscholas.glab30951.TestController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
 public class MyController {
+    @RequestMapping({"/login"})
+    public String showLogin() {
+        return "inboxpage";
+    }
+    @RequestMapping("/")
+    public String simpleMethod() {
+        return "inboxpage";
+    }
+    @RequestMapping("/index")
+    public String showIndex() {
+        return "inboxpage";
+    }
 }
--- src/main/java/com/perscholas/glab30951/controllerstwo/MyControllerTwo.java
+++ src/main/java/com/perscholas/glab30951/controllerstwo/MyControllerTwo.java
@@ -1,4 +1,14 @@
 package com.perscholas.glab30951.controllerstwo;
 
-public class MyControllerTwo {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyControllerTwo{
+    @GetMapping({ "login"})   // either type '/' or index
+    public String showIndex()
+    {
+        return "index";
+    }
 }
+
